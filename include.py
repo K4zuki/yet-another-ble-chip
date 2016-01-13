53522d2be0e43b65e1b05ba8503aa581b8643827
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2016-01-14 01:34:56 +0900

    correct argument parser description

--- include.py
+++ include.py
@@ -6,9 +6,7 @@ import sys
 import argparse
 class MyParser(object):
     def __init__(self):
-        self._parser = argparse.ArgumentParser(description = """take screenshot from Tek MSO/DPO/MDO4000 Oscilloscope,
-        will *Not* control instuments other than Oscillo
-        """)
+        self._parser = argparse.ArgumentParser(description = "cat some.txt| python include.py --out out_f.md")
         self._parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
         self._parser.add_argument('--out', '-O', help = 'output markdown file', default = "table.md")
         self.args = self._parser.parse_args(namespace=self)
